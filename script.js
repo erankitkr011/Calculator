aaa6135bb4ab2036e1e4e544ebd1c98d7e28e052
Ankit Kumar <drankitkr011@gmail.com>
2024-07-27 12:37:54 +0530

    Complete allmost

--- script.js
+++ script.js
@@ -1,12 +1,70 @@
-let input1 = document.querySelector("#input1");
-let input2 = document.querySelector("#input2");
-let result = document.querySelector("#result");
-let operate = document.querySelector("#operate");
+let input = document.getElementById("input");
+let numButtons = document.querySelectorAll(".numButton");
+let add = document.getElementById("add");
+let sub = document.getElementById("sub");
+let mul = document.getElementById("mul");
+let div = document.getElementById("div");
+let equal = document.getElementById("equal");
+let clear = document.getElementById("clear");
 
-result.value = "0";
-operate.addEventListener("click",function(){
-    let res = parseInt(input1.value) + parseInt(input2.value);
-    result.value=res;
-    input1.value=0;
-    input2.value=0;
-})
+let operation1 = null;
+let operation2 = null;
+let operator = null;
+
+numButtons.forEach((digit) => {
+    digit.addEventListener("click", digitButton);
+});
+add.addEventListener("click", () => setOperator("add"));
+sub.addEventListener("click", () => setOperator("sub"));
+mul.addEventListener("click", () => setOperator("mul"));
+div.addEventListener("click", () => setOperator("div"));
+equal.addEventListener("click", solution);
+clear.addEventListener("click", clearInput);
+
+function digitButton(event) {
+    let value = event.target.value;
+    input.value += value;
+
+    if (!operator) {
+        operation1 = parseInt(input.value);
+    } else {
+        operation2 = parseInt(input.value);
+    }
+}
+
+function setOperator(op) {
+    operator = op;
+    input.value = "";
+}
+
+function solution() {
+    let result;
+    switch (operator) {
+        case "add":
+            result = operation1 + operation2;
+            break;
+        case "sub":
+            result = operation1 - operation2;
+            break;
+        case "mul":
+            result = operation1 * operation2;
+            break;
+        case "div":
+            result = operation1 / operation2;
+            break;
+        default:
+            result = "Error";
+            break;
+    }
+    input.value = result;
+    operation1 = result;
+    operation2 = null;
+    operator = null;
+}
+
+function clearInput() {
+    input.value = "";
+    operation1 = null;
+    operation2 = null;
+    operator = null;
+}
